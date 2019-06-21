aaa3589520ea66cbc873ea4c8c1a058fdfdc95ce
Chris Wysocki <christopher.wysocki@gmail.com>
2019-06-21 14:48:34 -0700

    basic functionallity of weather API implimented

--- cst.js
+++ cst.js
@@ -7,6 +7,18 @@ var stateNameField = undefined;
 var stateCodeField = undefined;
 var searchField = undefined;
 
+function handleSearch() {
+	handleZipInput();
+
+	$('form').on('submit', function(event) {
+		event.preventDefault();
+		updateCitySearch(searchField);
+		// handleYelp();
+		// handleNews();
+		handleWeather();
+	});
+}
+
 function handleZipInput() {
 	// Search using Zip Code
 	$('#zip-input').keyup(function(event) {
@@ -33,29 +45,6 @@ function updateCitySearch(searchField) {
 	zipField = searchField.zip;
 }
 
-function handleSearch() {
-	handleZipInput();
-
-	$('form').on('submit', function(event) {
-		event.preventDefault();
-		updateCitySearch(searchField);
-		// handleYelp();
-		handleNews();
-	});
-}
-
-function searchYelp(yelpSettings) {
-	$.ajax(yelpSettings).done(function(response) {
-		console.log(response);
-	});
-}
-
-function searchNews(newsSettings) {
-	$.ajax(newsSettings).done(function(response) {
-		console.log(response);
-	});
-}
-
 function handleYelp() {
 	let token =
 		'Bearer kGyQVf1mVDbLkBJA3ybB37c2we4jmAPdkQfW-42BBsHskQQtW-zxsZIXpSY66UUVmAG7sG9_moAVW33smeq8OQbbU3JnvNu9bQviMTAZB4DMychkVCt0dW0WxX4JXXYx';
@@ -78,6 +67,12 @@ function handleYelp() {
 	searchYelp(yelpSettings);
 }
 
+function searchYelp(yelpSettings) {
+	$.ajax(yelpSettings).done(function(response) {
+		console.log(response);
+	});
+}
+
 function handleNews() {
 	let token = 'Bearer ba31778142b040128190f031a0b8a129';
 	var newsURL = 'https://newsapi.org/v2/everything';
@@ -95,6 +90,35 @@ function handleNews() {
 		}
 	};
 	searchNews(newsSettings);
+}
+
+function searchNews(newsSettings) {
+	$.ajax(newsSettings).done(function(response) {
+		console.log(response);
+	});
+}
+
+function handleWeather() {
+	let token = 'Bearer 2be7e49343b9a3ad3209acd251a978d0';
+	var weatherURL = 'api.openweathermap.org/data/2.5/weather';
+	var corsAnywhereURL = 'https://cors-anywhere.herokuapp.com';
+	var weatherSettings = {
+		async: true,
+		crossDomain: true,
+		url: corsAnywhereURL + '/' + weatherURL,
+		method: 'GET',
+		data: {
+			zip: zipField,
+			APPID: '2be7e49343b9a3ad3209acd251a978d0'
+		}
+	};
+	searchWeather(weatherSettings);
+}
+
+function searchWeather(weatherSettings) {
+	$.ajax(weatherSettings).done(function(response) {
+		console.log(response);
+	});
 }
 
 function handleCST() {
